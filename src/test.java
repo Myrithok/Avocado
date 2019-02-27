3d2b9bb20566928bdcd9ab3242e1f5ef281c8721
myrithok <mitchaj2@mcmaster.ca>
2019-02-27 11:11:26 -0500

    Testing git stuff

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@
 public class test {
 	public static void main(String[] args) {
 		System.out.println("Goodbye World");
-		System.out.println("Andrew test");
+		System.out.println("Andrew test number 2");
 	}
 	
 	public void printHello() {
