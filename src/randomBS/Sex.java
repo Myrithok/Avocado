ea45a70efd5426c0867f76b327402317855eff8c
Dakota Jonathan Mckay <mckaydj3@mcmaster.ca>
2019-04-03 17:02:20 -0400

    Added ability to get co-ordinate information from enum values.

--- src/randomBS/Sex.java
+++ src/randomBS/Sex.java
@@ -1,5 +1,18 @@
 package randomBS;
 
 public enum Sex {
-	Male,Female,PreferNotToSpecify
+	Male,Female,PreferNotToSpecify;
+	
+	public int toval() {
+		switch(this.name()) {
+			case "Male":
+				return 2;
+			case "Female":
+				return 3;
+			case "PreferNotToSpecify":
+				return 1;
+			default:
+				return 0;
+		}
+	}
 }
