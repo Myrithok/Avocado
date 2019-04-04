3434aad40b997fa2a2d0f119fba57a9057fd8735
Dakota Jonathan Mckay <mckaydj3@mcmaster.ca>
2019-04-04 12:21:57 -0400

    Added getters and setter for user class.
    Some getters and setters may be unecessary they can be deleted in the
    future.
    
    Comments to the functions in user next commit

--- src/randomBS/User.java
+++ src/randomBS/User.java
@@ -9,7 +9,7 @@ public class User {
 	private double medianDemographicDebt;
 	private double score;
 	private String username;
-	private double rank;
+	private int rank;
 	private int ID;
 	
 	private double debtAtGrad;
@@ -28,6 +28,172 @@ public class User {
 	private FieldOfStudy fieldOfStudy;
 	private Sex sex;
 	
+	private boolean hasChanged;
+	
+	public User() {
+		
+	}
+	
+	//getters and setters
+	
+	public double getMedianDemographicIncome() {
+		return this.medianDemographicIncome;
+	}
+	
+	public void setMedianDemographicIncome(double income) {
+		this.medianDemographicIncome = income;
+		this.hasChanged = true;
+	}
+	
+	public double getMedianDemographicDebt() {
+		return this.medianDemographicDebt;
+	}
+	
+	public void setMedianDemographicDebt(double debt) {
+		this.medianDemographicDebt = debt;
+		this.hasChanged = true;
+	}
+	
+	public double getScore() {
+		return this.score;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+		this.hasChanged = true;
+	}
+	
+	public int getRank() {
+		return this.rank;
+	}
+	
+	public void setRank(int rank) {
+		this.rank = rank;
+		this.hasChanged = true;
+	}
+	
+	public int getID() {
+		return this.ID;
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;
+		this.hasChanged = true;
+	}
+	
+	public double getDebtAtGrad() {
+		return this.debtAtGrad;
+	}
+	
+	public void setDebtAtGrad(double debtAtGrad) {
+		this.debtAtGrad = debtAtGrad;
+		this.hasChanged = true;
+	}
+	
+	public double getCurrentDebt() {
+		return this.currentDebt;
+	}
+	
+	public void setCurrentDebt(double currentDebt) {
+		this.currentDebt = currentDebt;
+		this.hasChanged = true;
+	}
+	
+	public double getInterestRate() {
+		return this.interestRate;
+	}
+	
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+		this.hasChanged = true;
+	}
+	
+	public Date getGradDate() {
+		return this.gradDate;
+	}
+	
+	public void setGradDate(Date gradDate) {
+		this.gradDate = gradDate;
+		this.hasChanged = true;
+	}
+	
+	public Date getDateOfBirth() {
+		return this.dateOfBirth;
+	}
+	
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+		this.hasChanged = true;
+	}
+	
+	public long getFriendCode() {
+		return this.friendCode;
+	}
+	
+	public void setFriendCode(long friendCode) {
+		this.friendCode = friendCode;
+		this.hasChanged = true;
+	}
+	
+	public boolean optedIn() {
+		return this.optedIn;
+	}
+	
+	public void optIn() {
+		this.optedIn = true;
+		this.hasChanged = true;
+	}
+	
+	public void optOut() {
+		this.optedIn = false;
+		this.hasChanged = true;
+	}
+	
+ 	public Sex getSex() {
+		return this.sex;
+	}
+	
+	public void setSex(Sex sex) {
+		this.sex = sex;
+		this.hasChanged = true;
+	}
+	
+	public Location getLocation() {
+		return this.geoLocation;
+	}
+	
+	public void setLocation(Location location) {
+		this.geoLocation = location;
+		this.hasChanged = true;
+	}
+	
+	public FieldOfStudy getFieldOfStudy() {
+		return this.fieldOfStudy;
+	}
+	
+	public void setFieldOfStudy(FieldOfStudy fieldOfStudy) {
+		this.fieldOfStudy = fieldOfStudy;
+		this.hasChanged = true;
+	}
+	
+	public EducationLevel getEduationLevel() {
+		return this.educationLevel;
+	}
+	
+	public void setEducationLevel(EducationLevel educationLevel) {
+		this.educationLevel = educationLevel;
+		this.hasChanged = true;
+	}
+ 	
+	public boolean hasChanged() {
+		return this.hasChanged;
+	}
+	
+	//main functions
 	public int getAgeAtGrad() {
 		LocalDate dateOfBirth = new java.sql.Date(this.dateOfBirth.getTime()).toLocalDate();
 		LocalDate gradDate = new java.sql.Date(this.gradDate.getTime()).toLocalDate();
